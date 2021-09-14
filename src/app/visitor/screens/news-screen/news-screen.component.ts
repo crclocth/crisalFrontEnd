ad2c12cd8e58474492dde352ee23bc7bd3851592
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-09-14 19:18:56 -0300

    feat(visitor): create news-screen component

---
+++ src/app/visitor/screens/news-screen/news-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-screen',
+  templateUrl: './news-screen.component.html',
+  styleUrls: ['./news-screen.component.less']
+})
+export class NewsScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
