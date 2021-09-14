80ef1aeef89bdb54708a2e11d864115c646ecf26
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-09-14 19:20:48 -0300

    feat(main): create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'visitor/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'visitor',
+    loadChildren: () =>
+      import('./visitor/visitor.module').then(
+        (VisitorModule) => VisitorModule.VisitorModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'visitor/home',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
