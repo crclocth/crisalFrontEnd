d0c8c561e2cbbbe59ffd4679477050710266f81e
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-09-14 19:17:51 -0300

    feat(visitor): create about-us-screen component

---
+++ src/app/visitor/screens/about-us-screen/about-us-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us-screen',
+  templateUrl: './about-us-screen.component.html',
+  styleUrls: ['./about-us-screen.component.less']
+})
+export class AboutUsScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
