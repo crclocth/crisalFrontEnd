52ef8765dd36f40db9000591a7956baa04219f5c
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-09-18 23:18:00 -0300

    feat(visitor routing): create children path visitor

--- src/app/visitor/visitor-routing.module.ts
+++ src/app/visitor/visitor-routing.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsScreenComponent } from './screens/about-us-screen/about-us-screen.component';
+import { AppointmentScreenComponent } from './screens/appointment-screen/appointment-screen.component';
+import { ContactScreenComponent } from './screens/contact-screen/contact-screen.component';
 import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { NewsScreenComponent } from './screens/news-screen/news-screen.component';
+import { ServicesScreenComponent } from './screens/services-screen/services-screen.component';
 import { VisitorModule } from './visitor.module';
 
 const routes: Routes = [
   {
     path: '',
     //component: VisitorModule,
-    children: [{ path: 'home', component: HomeScreenComponent }],
+    children: [
+      { path: 'inicio', component: HomeScreenComponent },
+      { path: 'quienes-somos', component: AboutUsScreenComponent },
+      { path: 'servicios', component: ServicesScreenComponent },
+      { path: 'agendar-hora', component: AppointmentScreenComponent },
+      { path: 'noticias', component: NewsScreenComponent },
+      { path: 'contactenos', component: ContactScreenComponent },
+    ],
   },
 ];
 
