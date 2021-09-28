ec40d0abe5994f16a353398840b354375d5c758c
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-09-27 21:16:04 -0300

    feat(about-us-screen): create history

--- src/app/visitor/screens/about-us-screen/about-us-screen.component.ts
+++ src/app/visitor/screens/about-us-screen/about-us-screen.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-us-screen',
   templateUrl: './about-us-screen.component.html',
-  styleUrls: ['./about-us-screen.component.less']
+  styleUrls: ['./about-us-screen.component.less'],
 })
 export class AboutUsScreenComponent implements OnInit {
+  public aboutUs: string;
+  public vision: string;
+  public mission: string;
+  public values: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.aboutUs =
+      'Nuestra empresa se basa en criterios de calidad en los servicios profesionales que ofrece a la industria y empresas de servicios, con la clara convicción de cumplir a cabalidad con los estándares de eficiencia que requiere y regula el mercado. Nuestra empresa se enmarca dentro de las políticas de calidad y seguridad en la información de los distintos servicios realizados, con el respaldo de certificaciones y registros para tales fines.';
+    this.vision =
+      'Ser una empresa líder en los servicio de salud ocupacional y ergonomía en la zona central del país con una visión clara de compromiso y calidad profesional';
+    this.mission =
+      'Ofrecer una atención personalizada, oportuna y efectiva de las necesidades de cada trabajador y empresas. Otorgar servicios de calidad y calidez a través de nuestros equipos de profesionales.';
+    this.values =
+      'Ser oportunos en la entrega de resultados de exámenes de los trabajadores a las empresas. Excelencia en el desempeño de sus profesionales y responsabilidad. Confiabilidad en la información oportuna';
   }
 
+  ngOnInit(): void {}
 }
