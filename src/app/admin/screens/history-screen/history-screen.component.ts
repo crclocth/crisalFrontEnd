ba481bad258ef1f41d961dc30f87172509c7a62c
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-09-29 18:34:58 -0300

    feat(admin): create history screen

---
+++ src/app/admin/screens/history-screen/history-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-history-screen',
+  templateUrl: './history-screen.component.html',
+  styleUrls: ['./history-screen.component.less']
+})
+export class HistoryScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
