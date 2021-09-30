75706194ae2db1e44f2da2edca5bea68ee96ef1c
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-09-30 19:21:31 -0300

    feat(certificate): create button

--- src/app/shared/components/certificate/certificate.component.ts
+++ src/app/shared/components/certificate/certificate.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-certificate',
   templateUrl: './certificate.component.html',
-  styleUrls: ['./certificate.component.less']
+  styleUrls: ['./certificate.component.less'],
 })
 export class CertificateComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public goToValidateCertificatesScreens(): void {
+    this.router.navigate(['/visitor/validar-certificado']);
   }
-
+  public goToDownloadCertificatesScreens(): void {
+    this.router.navigate(['/visitor/descargar-certificado']);
+  }
 }
