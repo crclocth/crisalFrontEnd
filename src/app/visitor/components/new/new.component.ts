099fed8fcd491842df79bb761e02889f62744d3c
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-09-30 19:23:26 -0300

    feat(new): create news

--- src/app/visitor/components/new/new.component.ts
+++ src/app/visitor/components/new/new.component.ts
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewComponent implements OnInit {
   public values: string;
+  public day: string;
+  public month: string;
+  public title: string;
 
   constructor() {
     this.values =
-      'Ser oportunos en la entrega de resultados de exámenes de los trabajadores a las empresas. Excelencia en el desempeño de sus profesionales y responsabilidad. Confiabilidad en la información oportuna';
+      'El mes de octubre CRISAL Salud Laboral está de aniversario, cumpliendo 5 años de atención de primera calidad en salud laboral. Queremos agradecer a todos los trabajadores y empresas que han confiado en nosotros y que gracias a su preferencia, hemos podido brindar más de 5 mil atenciones profesionales. Por ello, queremos celebrar este mes...';
+    this.day = '01';
+    this.month = 'OCT';
+    this.title = 'OCTUBRE: MES DE ANIVERSARIO';
   }
 
   ngOnInit(): void {}
