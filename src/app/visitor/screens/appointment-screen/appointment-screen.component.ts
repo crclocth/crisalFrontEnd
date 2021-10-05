769317896e0e2fd5201c52e6be8cbe30170b757f
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-10-05 16:53:01 -0300

    feat(appointment): create appointment component

--- src/app/visitor/screens/appointment-screen/appointment-screen.component.ts
+++ src/app/visitor/screens/appointment-screen/appointment-screen.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-screen',
   templateUrl: './appointment-screen.component.html',
-  styleUrls: ['./appointment-screen.component.less']
+  styleUrls: ['./appointment-screen.component.less'],
 })
 export class AppointmentScreenComponent implements OnInit {
+  public dummy: Number[];
+  public option: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.dummy = new Array(1);
+    this.option = '';
   }
 
+  ngOnInit(): void {}
+
+  /* addWorker() {
+    for (var i = 0; i < this.dummy.length; i++) {
+      this.dummy[i + 1] = this.dummy.push(1);
+      console.log(this.dummy[i]);
+    }
+  } */
+  clickCompany(op: string) {
+    if (op === 'yes') {
+      this.option = 'yes';
+    } else {
+      if (op === 'no') {
+        this.option = 'no';
+      }
+    }
+  }
 }
