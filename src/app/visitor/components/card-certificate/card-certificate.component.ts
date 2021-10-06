35381d25347903c7ad8c206ee431494c36ca1fb5
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-10-06 19:43:20 -0300

    feat(card-certificate): create card-certificate

---
+++ src/app/visitor/components/card-certificate/card-certificate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-certificate',
+  templateUrl: './card-certificate.component.html',
+  styleUrls: ['./card-certificate.component.less']
+})
+export class CardCertificateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
