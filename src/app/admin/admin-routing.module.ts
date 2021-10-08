e103b8124f462a5d5cc0edd414da806dbc5adeac
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-10-08 18:19:52 -0300

    feat(admin-routes): creat path

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeScreenComponent } from '../admin/screens/home-screen/home-screen.component';
+import { HistoryScreenComponent } from './screens/history-screen/history-screen.component';
 import { LoginComponent } from './screens/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: [{ path: 'login', component: LoginComponent }],
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'historial-certificados', component: HistoryScreenComponent },
+      { path: 'inicio', component: HomeScreenComponent },
+    ],
   },
 ];
 
