d7a1ef85d03c1f3755fdcee88d3285be79dbf6f4
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-10-08 18:18:58 -0300

    chore(main): import material sidebar

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,11 +5,33 @@ import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './screens/login/login.component';
 import { AdminComponent } from './admin.component';
 import { HistoryScreenComponent } from './screens/history-screen/history-screen.component';
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { AccordionComponent } from './components/accordion/accordion.component';
 
-const components = [LoginComponent, AdminComponent];
+const components = [
+  LoginComponent,
+  AdminComponent,
+  HistoryScreenComponent,
+  HomeScreenComponent,
+  AccordionComponent,
+];
 
 @NgModule({
-  declarations: [...components, HistoryScreenComponent],
-  imports: [CommonModule, AdminRoutingModule, SharedModule],
+  declarations: [...components],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    SharedModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
 })
 export class AdminModule {}
