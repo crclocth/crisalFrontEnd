7254b72706c658570acf0d2a0640d0a419c60844
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-10-16 21:19:44 -0300

    feat(admin): create login-screen

---
+++ src/app/visitor/screens/login-screen/login-screen.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-screen',
+  templateUrl: './login-screen.component.html',
+  styleUrls: ['./login-screen.component.less'],
+})
+export class LoginScreenComponent {
+  public hasUnitNumber: boolean;
+  public addressForm: FormGroup;
+
+  constructor(private fb: FormBuilder, public router: Router) {
+    this.addressForm = this.fb.group({
+      user: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ],
+      ],
+      password: ['', [Validators.required]],
+    });
+    this.hasUnitNumber = false;
+  }
+
+  get user() {
+    return this.addressForm.get('user')?.value;
+  }
+  get password() {
+    return this.addressForm.get('password')?.value;
+  }
+
+  public goToHistoryScreen(): void {
+    this.router.navigate(['/admin']);
+  }
+
+  onSubmit(): void {
+    alert('Thanks!');
+  }
+}
