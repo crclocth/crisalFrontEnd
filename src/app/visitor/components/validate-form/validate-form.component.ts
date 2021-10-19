fb2164a8e7363c80de001e3ecc356ba85e0a3934
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-10-19 12:52:37 -0300

    feat(visitor): fix title validate

--- src/app/visitor/components/validate-form/validate-form.component.ts
+++ src/app/visitor/components/validate-form/validate-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { rutTools } from 'prettyutils';
 
@@ -40,6 +40,10 @@ export class ValidateFormComponent {
     console.log(result, this.addressForm.valid);
   }
 
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?: any) {
+    return window.innerWidth;
+  }
   onSubmit(): void {
     alert('Thanks!');
   }
