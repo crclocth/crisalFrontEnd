c63272262c823546033e21059949e253e1854bff
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-10-23 21:18:03 -0300

    feat(core): create information service

---
+++ src/app/core/models/information.model.ts
@@ -0,0 +1,11 @@
+export interface Information {
+  _id: string;
+  telephone1: number;
+  telephone2: number;
+  address: string;
+  mail: string;
+  aboutUs: string;
+  vision: string;
+  mission: string;
+  values: string;
+}
