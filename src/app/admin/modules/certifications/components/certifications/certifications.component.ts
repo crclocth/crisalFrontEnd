6b1cd95d3c9ff403b675164cb5fa70f4efcb2ca3
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-11-03 22:02:23 -0300

    feat(admin/modules): create certifications

--- src/app/admin/modules/certifications/components/certifications/certifications.component.ts
+++ src/app/admin/modules/certifications/components/certifications/certifications.component.ts
@@ -1,5 +1,16 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Input,
+  OnInit,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Certification } from 'src/app/core/models/certification.model';
+import { DeleteModalComponent } from '../delete-modal/delete-modal.component';
+import { EditModalComponent } from '../edit-modal/edit-modal.component';
+import { SeeModalComponent } from '../see-modal/see-modal.component';
 
 @Component({
   selector: 'app-certifications',
@@ -8,9 +19,44 @@ import { Certification } from 'src/app/core/models/certification.model';
 })
 export class CertificationsComponent implements OnInit {
   @Input() certification!: Certification;
-  constructor() {}
+  @Output() newItemEvent = new EventEmitter<string>();
+
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit(): void {}
+
+  openModalDelete() {
+    const modalRef = this.modalService.open(DeleteModalComponent, {
+      size: 'lg',
+    });
+    modalRef.componentInstance.certification = this.certification;
+    modalRef.result.then((result) => {
+      console.log(result);
+      this.newItemEvent.emit(result);
+    });
+  }
+
+  openModalEdit() {
+    const modalRef = this.modalService.open(EditModalComponent, {
+      size: 'lg',
+    });
+    modalRef.componentInstance.certification = this.certification;
+    modalRef.result.then((result) => {
+      console.log(result);
+      this.newItemEvent.emit(result);
+    });
+  }
+
+  openModalSee() {
+    const modalRef = this.modalService.open(SeeModalComponent, {
+      size: 'lg',
+    });
+    modalRef.componentInstance.certification = this.certification;
+    modalRef.result.then((result) => {
+      console.log(result);
+      this.newItemEvent.emit(result);
+    });
+  }
 
   @HostListener('window:resize', ['$event'])
   getScreenSize(event?: any) {
