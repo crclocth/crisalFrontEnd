d1538ab88b279be6b3172e4758770e12f90f89e2
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-11-03 22:04:04 -0300

    feat(admin/modules/client): delete-modal

---
+++ src/app/admin/modules/clients/components/delete-modal/delete-modal.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ClientProviderService } from 'src/app/core/providers/client/client-provider.service';
+import { NotificationService } from 'src/app/core/services/notification/notification.service';
+
+@Component({
+  selector: 'app-delete-modal',
+  templateUrl: './delete-modal.component.html',
+  styleUrls: ['./delete-modal.component.less'],
+})
+export class DeleteModalComponent implements OnInit {
+  public client: any;
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    private celientProviderService: ClientProviderService,
+    private notificationService: NotificationService
+  ) {}
+
+  ngOnInit(): void {}
+
+  async delete() {
+    try {
+      await this.celientProviderService.deleteClientById(this.client._id);
+      this.notificationService.success('Se Eliminó el Cliente');
+      this.activeModal.close('info modal');
+    } catch (error) {
+      this.notificationService.error('Error al Eliminar al Cliente');
+    }
+  }
+}
