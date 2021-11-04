c018222f4254f9d52c60d61fa663a9f977f632f3
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-11-03 22:05:55 -0300

    feat(admin/modules/companies): see-modal

---
+++ src/app/admin/modules/companies/components/see-modal/see-modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-see-modal',
+  templateUrl: './see-modal.component.html',
+  styleUrls: ['./see-modal.component.less'],
+})
+export class SeeModalComponent implements OnInit {
+  public company: any;
+  constructor(public activeModal: NgbActiveModal) {}
+
+  ngOnInit(): void {
+    console.log(this.company);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?: any) {
+    return window.innerWidth;
+  }
+}
