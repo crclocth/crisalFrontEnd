bd56ac08411a4da861e73131b2700ac297dc33d7
Cristian Reveco <cristian.reveco.v@mail.pucv.cl>
2021-11-04 21:42:02 -0300

    feat(admin/modules/battery): delete-modal

---
+++ src/app/admin/modules/batteries/components/delete-modal/delete-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-modal',
+  templateUrl: './delete-modal.component.html',
+  styleUrls: ['./delete-modal.component.less']
+})
+export class DeleteModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
